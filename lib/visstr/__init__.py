fe80471b9520fb4cc0eba43231fa5e0572920350
kg86 <kg86@users.noreply.github.com>
2021-07-25 12:45:23 +0900

    fix: the first canvas is unvisible

--- lib/visstr/__init__.py
+++ lib/visstr/__init__.py
@@ -1,35 +1,31 @@
 import os
-from IPython.display import HTML, display
+from IPython.display import HTML, display, Javascript
 
 num_canvas = 0
 
-visstr_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "vis_str.umd.js")
-# print("visstr_path", visstr_path)
+lib_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "vis_str.umd.js")
+lib_src = open(lib_path, "r").read()
 
-vislib = "<script>{}</script>".format(open(visstr_path, "r").read())
-
-display(HTML(vislib))
+display(Javascript(lib_src))
 
 
 def visstr(s, ranges):
     template = """
-    <canvas id="canvas_{}" style="border: 1px solid #000000; background-color: white"></canvas>
-    <script>
-    function main(){{
-      const canvas = document.getElementById('canvas_{}')
-      const s = '{}'
+    <canvas id="visstr_canvas_{}" style="border: 1px solid #000000; background-color: white"></canvas>
+    <script type="text/javascript">
+    (function (){{
+      const canvas = document.getElementById("visstr_canvas_{}")
+      const s = "{}"
       const vstr = new visstr.VisStr(canvas)
-      const range_groups = vstr.makeGroupRangesAutoColor({}, 'arrow')
+      const range_groups = vstr.makeGroupRangesAutoColor({}, "arrow")
       const ranges = range_groups
         .map((x) => vstr.nonOverlapRanges(x))
         .reduce((acm, x) => acm.concat(x), [])
       vstr.draw(s, ranges)
-    }}
-    main()
+    }})()
     </script>
     """
     global num_canvas
     html = template.format(num_canvas, num_canvas, s, ranges)
     num_canvas += 1
-    # print(num_canvas)
     return HTML(html)
