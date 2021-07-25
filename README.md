780e3cb0e93610a358617e69a945a1effc0a2ad9
kg86 <kg86@users.noreply.github.com>
2021-07-25 19:01:25 +0900

    update README

--- README.md
+++ README.md
@@ -2,14 +2,14 @@ Note: this library is under developping, so its API may be changed in future.
 
 # String Visualizer for Jupyter notebook
 
-This is python binding of string visualizer [visstr](https://github.com/kg86/visstr) written in javascript.
+This is a python binding of string visualizer [visstr](https://github.com/kg86/visstr) written in javascript, which work on Jupyter notebook.
 
 ![](others/example.png)
 
 # Install
 
 ```bash
-$ pip install .
+$ pip install git+https://github.com/kg86/visstr-py
 ```
 
 # Example
--- lib/visstr/__init__.py
+++ lib/visstr/__init__.py
@@ -1,5 +1,10 @@
 import os
+import uuid
 from IPython.display import HTML, display, Javascript
+
+lib_local = os.path.join(os.path.dirname(os.path.abspath(__file__)), "vis_str.umd.js")
+lib_url = "https://unpkg.com/visstr@0.0.5/lib/vis_str.umd.js"
+lib_tag = f'<script type="text/javascript" src="{lib_url}"></script>'
 
 try:
     import google.colab
@@ -8,30 +13,34 @@ try:
 except:
     IN_COLAB = False
 
-num_canvas = 0
-
-lib_local = os.path.join(os.path.dirname(os.path.abspath(__file__)), "vis_str.umd.js")
-lib_url = "https://unpkg.com/visstr@0.0.5/lib/vis_str.umd.js"
-lib_path = lib_url if IN_COLAB else lib_local
+if not IN_COLAB:
+    display(Javascript(open(lib_local, "r").read()))
 
 
-def visstr(s, ranges):
+def Str(s, ranges):
     global num_canvas
-    html = f"""
-    <script type="text/javascript" src="{lib_path}"></script>
-    <canvas id="visstr_canvas_{num_canvas}" style="border: 1px solid #000000; background-color: white"></canvas>
+
+    canvas_id = uuid.uuid4()
+    img_id = uuid.uuid4()
+    html = lib_tag if IN_COLAB else ""
+
+    html += f"""
+    <canvas id="{canvas_id}" style="display:none; border: 1px solid #000000; background-color: white"></canvas>
+    <img id="{img_id}" />
     <script type="text/javascript">
     (function (){{
-      const canvas = document.getElementById("visstr_canvas_{num_canvas}")
+      const canvas = document.getElementById("{canvas_id}")
       const s = "{s}"
-      const vstr = new visstr.VisStr(canvas)
+      const vstr = new visstr.VisStr(canvas, 64)
       const range_groups = vstr.makeGroupRangesAutoColor({ranges}, "arrow")
       const ranges = range_groups
         .map((x) => vstr.nonOverlapRanges(x))
         .reduce((acm, x) => acm.concat(x), [])
       vstr.draw(s, ranges)
+      const img = document.getElementById("{img_id}")
+      img.src = canvas.toDataURL("image/png")
+
     }})()
     </script>
     """
-    num_canvas += 1
     return HTML(html)
